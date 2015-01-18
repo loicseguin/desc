71c31d4ed17e4e7a6fffd183aef3e81788e10ea8
Loïc Séguin-C <lsc@loicseguin.com>
2015-01-17 22:30:46 -0500

    Add usage

--- desc.c
+++ desc.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "dbg.h"
 #include "stats.h"
 
+void usage()
+{
+    fprintf(stderr,
+            "usage: desc DATAFILE\n\n"
+            "desc analyse the data in DATAFILE and prints summary statistics\n"
+            "that describe the data.\n\n"
+            "The input must consist of one number per line. If no DATAFILE is\n"
+            "provided, desc reads from standard input which means it can be\n"
+            "used with pipes.\n\n"
+            "Examples\n"
+            "--------\n"
+            "cat data/large.dat | desc\n"
+            );
+
+    exit(1);
+}
+
+
 int main(int argc, char *argv[])
 {
-    dataset *ds = read_data_file(argv[1]);
-    if (!ds)
+    int ch;
+
+	while ((ch = getopt(argc, argv, "h")) != -1)
+		switch (ch) {
+		case 'h':
+            usage();
+			break;
+		default:
+			usage();
+		}
+	argv += optind;
+
+    dataset *ds = read_data_file(argv[0]);
+    if (!ds) {
+        fprintf(stderr, "\n");
+        debug("Bouyya!");
+        usage();
         return 1;
+    }
 
     printf("count     %zu\n", ds->n);
     printf("min       %.5g\n", min(ds));
