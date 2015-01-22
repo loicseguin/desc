739c6d6c887ddec1a5fefbb377dcdecc143361ba
Loïc Séguin-C <lsc@loicseguin.com>
2015-01-22 15:04:27 -0500

    Update README and help message.

--- desc.c
+++ desc.c
@@ -8,12 +8,18 @@
 void usage()
 {
     fprintf(stderr,
-            "usage: desc DATAFILE\n\n"
+            "usage: desc [-hs] DATAFILE\n\n"
             "desc analyse the data in DATAFILE and prints summary statistics\n"
             "that describe the data.\n\n"
             "The input must consist of one number per line. If no DATAFILE is\n"
             "provided, desc reads from standard input which means it can be\n"
             "used with pipes.\n\n"
+            "Options\n"
+            "-------\n"
+            "-h    Print this usage message and exit.\n\n"
+            "-s    Run in streaming mode. This uses almost no memory and run \n"
+            "      time scales linearly with input size. However, the percentiles\n"
+            "      are calculated approximately.\n\n"
             "Examples\n"
             "--------\n"
             "cat data/large.dat | desc\n"
